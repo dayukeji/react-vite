18a5496455d317ec5f1dda866b3585636a4e9845
dayukeji <huliml@126.com>
2022-11-03 19:56:41 +0800

    3 files modified, 4 added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import styleImport from 'vite-plugin-style-import';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [
+    react(),
+    // styleImport({
+    //   libs: [
+    //     {
+    //       libraryName: 'zarm',
+    //       esModule: true,
+    //       resolveStyle: (name) => {
+    //         return `zarm/es/${name}/style/css`;
+    //       },
+    //     },
+    //   ],
+    // }),
+  ],
+});
