81aa81a5b0e4491bd29e9fadc2bb7adebd2a43de
dayukeji <huliml@126.com>
2022-11-05 10:59:43 +0800

    2 files modified, 2 added

--- src/container/About/index.jsx
+++ src/container/About/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Button } from 'zarm';
+import s from './style.module.less'
 
 const About = () => {
   return (
-    <div>
+    <div className={s.about}>
+      <span>样式</span>
       <Button theme="primary">Hello!</Button>
     </div>
   );
