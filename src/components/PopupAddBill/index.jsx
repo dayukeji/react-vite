1d4dc5df12d95dccfd590d4b46936708a631266d
dayukeji <huliml@126.com>
2022-11-11 01:03:02 +0800

    1 file modified

--- src/components/PopupAddBill/index.jsx
+++ src/components/PopupAddBill/index.jsx
@@ -1,18 +1,26 @@
 import React, { forwardRef, useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
-import {Popup, Icon, Keyboard} from 'zarm';
+import {Popup, Icon, Keyboard,Input,Toast} from 'zarm';
 import dayjs from 'dayjs';
 import s from './style.module.less';
+import CustomIcon from '../CustomIcon';
+import { get, post, typeMap } from '@/utils';
 import PopupDate from '../PopupDate'
 
-const PopupAddBill = forwardRef((props, ref) => {
+const PopupAddBill = forwardRef(({detail={}, onReload}, ref) => {
+  const id = detail && detail.id // 外部传进来的账单详情 id
   const [show, setShow] = useState(false) // 内部控制弹窗显示隐藏
   const [payType, setPayType] = useState('expense'); // 支出或收入类型
   const dateRef = useRef();
   const [date, setDate] = useState(new Date()); // 日期
   const [amount, setAmount] = useState('');
+  const [currentType, setCurrentType] = useState({}); // 当前选中账单类型
+  const [expense, setExpense] = useState([]); // 支出类型数组
+  const [income, setIncome] = useState([]); // 收入类型数组
 
+  const [remark, setRemark] = useState(''); // 备注
+  const [showRemark, setShowRemark] = useState(false); // 备注输入框展示控制
 
   // 通过 forwardRef 拿到外部传入的 ref，并添加属性，使得父组件可以通过 ref 控制子组件
   if (ref) {
@@ -49,6 +57,7 @@ const PopupAddBill = forwardRef((props, ref) => {
     // 点击确认按钮时
     if (value == 'ok') {
       // 这里后续将处理添加账单逻辑
+      addBill()
       return
     }
 
@@ -59,6 +68,67 @@ const PopupAddBill = forwardRef((props, ref) => {
     // amount += value
     setAmount(amount + value)
   }
+
+
+  // 添加账单
+  const addBill = async () => {
+    if (!amount) {
+      Toast.show('请输入具体金额')
+      return
+    }
+    const params = {
+      amount: Number(amount).toFixed(2), // 账单金额小数点后保留两位
+      type_id: currentType.id, // 账单种类id
+      type_name: currentType.name, // 账单种类名称
+      date: dayjs(date).unix() * 1000, // 日期传时间戳
+      pay_type: payType == 'expense' ? 1 : 2, // 账单类型传 1 或 2
+      remark: remark || '' // 备注
+    }
+    if (id) {
+      params.id = id;
+      // 如果有 id 需要调用详情更新接口
+      const result = await post('/bill/update', params);
+      Toast.show('修改成功');
+    } else {
+      const result = await post('/bill/add', params);
+      setAmount('');
+      setPayType('expense');
+      setCurrentType(expense[0]);
+      setDate(new Date());
+      setRemark('');
+      Toast.show('添加成功');
+    }
+    setShow(false);
+    onReload && onReload();
+  }
+
+  useEffect(() => {
+    if (detail.id) {
+      setPayType(detail.pay_type == 1 ? 'expense' : 'income')
+      setCurrentType({
+        id: detail.type_id,
+        name: detail.type_name
+      })
+      setRemark(detail.remark)
+      setAmount(detail.amount)
+      setDate(dayjs(Number(detail.date)).$d)
+    }
+  }, [detail])
+
+  useEffect( () => {
+    async function handleMount(){
+      const { data: { list } } = await get('/type/list');
+      const _expense = list.filter(i => i.type == 1); // 支出类型
+      const _income = list.filter(i => i.type == 2); // 收入类型
+      setExpense(_expense);
+      setIncome(_income);
+      // 没有 id 的情况下，说明是新建账单。
+      if (!id) {
+        setCurrentType(_expense[0]);
+      };
+    }
+    handleMount()
+  }, [])
 
   return <Popup
     visible={show}
@@ -86,6 +156,35 @@ const PopupAddBill = forwardRef((props, ref) => {
       <div className={s.money}>
         <span className={s.sufix}>¥</span>
         <span className={cx(s.amount, s.animation)}>{amount}</span>
+      </div>
+      <div className={s.typeWarp}>
+        <div className={s.typeBody}>
+          {/* 通过 payType 判断，是展示收入账单类型，还是支出账单类型 */}
+          {
+            (payType == 'expense' ? expense : income).map(item => <div onClick={() => setCurrentType(item)} key={item.id} className={s.typeItem}>
+              {/* 收入和支出的字体颜色，以及背景颜色通过 payType 区分，并且设置高亮 */}
+              <span className={cx({[s.iconfontWrap]: true, [s.expense]: payType == 'expense', [s.income]: payType == 'income', [s.active]: currentType.id == item.id})}>
+                <CustomIcon className={s.iconfont} type={typeMap[item.id].icon} />
+              </span>
+              <span>{item.name}</span>
+            </div>)
+          }
+        </div>
+      </div>
+      <div className={s.remark}>
+        {
+          showRemark ? <Input
+            autoHeight
+            showLength
+            maxLength={50}
+            type="text"
+            rows={3}
+            value={remark}
+            placeholder="请输入备注信息"
+            onChange={(val) => setRemark(val)}
+            onBlur={() => setShowRemark(false)}
+          /> : <span onClick={() => setShowRemark(true)}>{remark || '添加备注'}</span>
+        }
       </div>
       <Keyboard type="price" onKeyClick={(value) => handleMoney(value)} />
       <PopupDate ref={dateRef} onSelect={selectDate} />
