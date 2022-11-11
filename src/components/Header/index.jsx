59b40a7e1b21661ca661e5fe47df898ce0927fd7
dayukeji <huliml@126.com>
2022-11-11 20:52:07 +0800

    2 files modified, 9 added

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// import { useNavigate } from 'react-router-dom'
+import { NavBar, Icon } from 'zarm';
+
+import s from './style.module.less';
+
+const Header = ({ title = '' }) => {
+  // const navigateTo = useNavigate()
+  return (
+    <div className={s.headerWarp}>
+      <div className={s.block}>
+        <NavBar
+          className={s.header}
+          left={
+            <Icon
+              type="arrow-left"
+              theme="primary"
+              onClick={() => window.history.back()}
+            />
+          }
+          title={title}
+        />
+      </div>
+    </div>
+  );
+};
+
+Header.propTypes = {
+  title: PropTypes.string, // 标题
+};
+
+export default Header;
---
+++ src/components/Nav/index.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { TabBar } from 'zarm';
+import { useHistory, useLocation } from 'react-router-dom';
+import CustomIcon from '../CustomIcon';
+
+import s from './style.module.less';
+
+const NavBar = ({ showNav }) => {
+  const [activeKey, setActiveKey] = useState(useLocation().pathname);
+
+  const history = useHistory();
+  const changeTab = (path) => {
+    setActiveKey(path);
+    history.push(path);
+  };
+  return (
+    <>
+      <TabBar visible={showNav} activeKey={activeKey} onChange={changeTab}>
+        <TabBar.Item
+          itemKey="/"
+          title="账单"
+          icon={<CustomIcon type="zhangdan" />}
+        />
+        <TabBar.Item
+          itemKey="/data"
+          title="统计"
+          icon={<CustomIcon type="tongji" />}
+        />
+        <TabBar.Item
+          itemKey="/user"
+          title="我的"
+          icon={<CustomIcon type="wode" />}
+        />
+      </TabBar>
+    </>
+  );
+};
+NavBar.propTypes = {
+  showNav: PropTypes.bool,
+};
+
+export default NavBar;
